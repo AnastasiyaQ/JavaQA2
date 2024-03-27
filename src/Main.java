12471efd5cd81ca97c59d0d20dd28698aea33a6b
Anastasia <pilipenkoanastasiyaolegovna@yandex.ru>
2024-03-27 19:00:35 +0300

    changed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10_000;
+        int spent = 20;
+        int miles = amount / spent;
+        System.out.println("Количество бонуных миль " + miles);
+    }
+}
+
+
